89de61be86cb2825561a1079c08d13596f9e29af
Łukasz Magiera <magik6k@gmail.com>
2017-11-19 02:43:58 +0100

    dag: fix provide logic
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- core/commands/dag/dag.go
+++ core/commands/dag/dag.go
@@ -137,7 +137,7 @@ into an object of the specified format.
 
 				cids.ForEach(func(c *cid.Cid) error {
 					n.Pinning.PinWithMode(c, pin.Recursive)
-					return n.Providers.Provide(c)
+					return nil
 				})
 
 				err := n.Pinning.Flush()
@@ -146,7 +146,9 @@ into an object of the specified format.
 				}
 			}
 
-			return nil
+			return cids.ForEach(func(c *cid.Cid) error {
+				return n.Providers.Provide(c)
+			})
 		}
 
 		go func() {
